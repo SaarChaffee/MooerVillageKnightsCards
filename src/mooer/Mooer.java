92d8b57337b65f6fbbdafe19df4d78992521a695
SaarChaffee <1240148332@qq.com>
2021-06-17 19:27:47 +0800

    load CardsCreat() when only server run

--- src/mooer/Mooer.java
+++ src/mooer/Mooer.java
@@ -26,14 +26,14 @@ public class Mooer {
         //new Card_Map_6().setVisible(true);
         //new Store().setVisible(true);
         //new Store_Gacha().setVisible(true);
-        new SignIn().setVisible( true );
+//        new SignIn().setVisible( true );
 //        new play();//播放音乐
 //            new Music();
 
         //     new Main_UI().setVisible(true);
-
+        new CardsCreat();
         new Server().setVisible( true );
-        //  new CtoS("127.0.0.1").send("nmsl");
+//          new CtoS("127.0.0.1").send("nmsl");
 
 // new Register().setVisible(true);
         //   ChangeImg.changeImg(ImgTest.jLabel1, 8);
